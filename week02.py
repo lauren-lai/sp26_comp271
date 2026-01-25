57e9094c750a18454983363212a94f0e36339495
lauren lai <97567505+lauren-lai@users.noreply.github.com>
2026-01-25 11:29:14 -0600

    refactored remove_all_first_name to use remove_first_name

--- week02.py
+++ week02.py
@@ -22,14 +22,14 @@ class Week02:
         """
         creates a new entry in underlying of [first_name, last_name, role] if first_name isnt in underlying[0]
         """
-        if not contains_name(first_name, underlying, 0):
+        if not self.contains_name(first_name, underlying, 0):
             underlying.append([first_name, last_name, role])
     
     def add_unique_last_name(self, first_name: str, last_name:str, role: str,underlying: list[list[str]]) -> None:
         """
         creates a new entry in underlying of [first_name, last_name, role] if last_name isnt in underlying[1]
         """
-        if not contains_name(last_name, underlying, 1):
+        if not self.contains_name(last_name, underlying, 1):
             underlying.append([first_name, last_name, role])
     
     def remove_first_name(self, first_name: str, underlying: list[list[str]]) -> str|None:
@@ -41,26 +41,24 @@ class Week02:
         i: int = 0
         while (not have_removed) and (i < len(underlying)):
             if underlying[i][0] == first_name:
-                removed = underlying[i][0]
+                removed = underlying.pop(i)
                 have_removed = True
             i += 1
         return removed
         
-
     def remove_all_first_name(self, first_name: str, underlying: list[list[str]]) -> list[str]|None:
         """
         removes all instances of first_name from underlying and returns a list of all instances, otherwise returns None
         """
-        removed: list = []
+        removed_names: list = []
         i: int = 0
         while i < len(underlying):
-            if underlying[i][0] == first_name:
-                removed.append(underlying[i][0])
-                underlying.pop(i)
+            name = self.remove_first_name(first_name, underlying)
+            if name != None:
+                removed_names.append(name)
             else:
                 i += 1
-        return removed
-
+        return removed_names
 
 class Main():
     _ST_CHARACTERS = [
@@ -69,12 +67,18 @@ class Main():
         ["Dustin", "Henderson", "Science Enthusiast"]
     ]
     week02 = Week02()
-
-    #TODO: refactor the remove methods
+    print(f"ORIGINAL LIST = {_ST_CHARACTERS}")
+    
     week02.add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    week02.add_unique_first_name("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    week02.add_unique_last_name("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    week02.remove_first_name("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    week02.remove_first_name("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    week02.add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    week02.add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    print(f"ADDEDD NAMES = {_ST_CHARACTERS}")
 
+    # week02.add_unique_first_name("Steve", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    # week02.add_unique_last_name("Steve", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    # week02.remove_first_name("Yuri", _ST_CHARACTERS)
+    removed_list = week02.remove_all_first_name("Yuri", _ST_CHARACTERS)
+    print(removed_list)
+
+    print(f"FINAL LIST = {_ST_CHARACTERS}")
 
