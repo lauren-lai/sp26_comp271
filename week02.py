f4db377c6593c713893b31173f59e9459534f235
lauren lai <97567505+lauren-lai@users.noreply.github.com>
2026-01-30 11:38:28 -0600

    actual final ver for week02 removed class stuff

--- week02.py
+++ week02.py
@@ -1,89 +1,87 @@
-class Week02:
-    def __init__(self):
-        self._FIRST_NAME = 0
-        self._LAST_NAME = 1
-        self._ROLE = 2
+_FIRST_NAME = 0
+_LAST_NAME = 1
+_LAST_NAMEROLE = 2
 
-    def _contains_name(self, name: str, underlying: list[list[str]], index: int) -> bool:
-        """
-        returns True if underlying contains name in the sublist index of underlying, False otherwise
-        """
-        is_found: bool = False
-        i: int = 0
-        while (not is_found) and (i < len(underlying)):
-            if (underlying[i][index] == name):
-                is_found = True
-            i += 1
-        return is_found
+def _contains_name(name: str, underlying: list[list[str]], index: int) -> bool:
+    """
+    returns True if underlying contains name in the sublist index of underlying, False otherwise
+    """
+    is_found: bool = False
+    i: int = 0
+    while (not is_found) and (i < len(underlying)):
+        if (underlying[i][index] == name):
+            is_found = True
+        i += 1
+    return is_found
 
-    def add(self, first_name: str, last_name :str, role: str, underlying: list[list[str]]) -> None:
-        """
-        creates a new entry in underlying of [first_name, last_name, role]
-        """
-        underlying.append([first_name, last_name, role])
+def add(first_name: str, last_name :str, role: str, underlying: list[list[str]]) -> None:
+    """
+    creates a new entry in underlying of [first_name, last_name, role]
+    """
+    underlying.append([first_name, last_name, role])
     
-    def add_unique_first_name(self, first_name: str, last_name: str, role: str, underlying: list[list[str]]) -> None:
+def add_unique_first_name(first_name: str, last_name: str, role: str, underlying: list[list[str]]) -> None:
         """
         creates a new entry in underlying of [first_name,  last_name, role] if first_name isnt in underlying[_FIRST_NAME]
         """
-        if not self._contains_name(first_name, underlying, self._FIRST_NAME):
+        if not _contains_name(first_name, underlying, _FIRST_NAME):
             underlying.append([first_name, last_name, role])
     
-    def add_unique_last_name(self, first_name: str, last_name: str, role: str,underlying: list[list[str]]) -> None:
-        """
-        creates a new entry in underlying of [first_name, last_name, role] if last_name isnt in underlying[_LAST_NAME]
-        """
-        if not self._contains_name( last_name, underlying, self._LAST_NAME):
-            underlying.append([first_name, last_name, role])
+def add_unique_last_name(first_name: str, last_name: str, role: str,underlying: list[list[str]]) -> None:
+    """
+    creates a new entry in underlying of [first_name, last_name, role] if last_name isnt in underlying[_LAST_NAME]
+    """
+    if not _contains_name( last_name, underlying, _LAST_NAME):
+        underlying.append([first_name, last_name, role])
     
-    def remove_first_name(self, first_name: str, underlying: list[list[str]]) -> str|None:
-        """
-        removes first_name from underlying if present and returns first_name, otherwise returns None
-        """
-        removed: str = None
-        have_removed: bool = False
-        i: int = 0
-        while (not have_removed) and (i < len(underlying)):
-            if underlying[i][self._FIRST_NAME] == first_name:
-                removed = underlying.pop(i)
-                have_removed = True
-            i += 1
-        return removed
+def remove_first_name(first_name: str, underlying: list[list[str]]) -> str|None:
+    """
+    removes first_name from underlying if present and returns first_name, otherwise returns None
+    """
+    removed: str = None
+    have_removed: bool = False
+    i: int = 0
+    while (not have_removed) and (i < len(underlying)):
+        if underlying[i][_FIRST_NAME] == first_name:
+            removed = underlying.pop(i)
+            have_removed = True
+        i += 1
+    return removed
         
-    def remove_all_first_name(self, first_name: str, underlying: list[list[str]]) -> list[str]|None:
-        """
-        removes all instances of first_name from underlying and returns a list of all instances, otherwise returns None
-        """
-        removed_names: list = []
-        i: int = 0
-        while i < len(underlying):
-            name = self.remove_first_name(first_name, underlying)
-            if name != None:
-                removed_names.append(name)
-            else:
-                i += 1
-        return removed_names if len(removed_names) > 0 else None
+def remove_all_first_name(first_name: str, underlying: list[list[str]]) -> list[str]|None:
+    """
+    removes all instances of first_name from underlying and returns a list of all instances, otherwise returns None
+    """
+    removed_names: list = []
+    i: int = 0
+    while i < len(underlying):
+        name = remove_first_name(first_name, underlying)
+        if name != None:
+            removed_names.append(name)
+        else:
+            i += 1
+    return removed_names if len(removed_names) > 0 else None
 
-class Main():
+def main():
     _ST_CHARACTERS = [
         ["Jim", "Hopper", "Chief of Police"],
         ["Eleven", "", "Psychokinetic Overachiever"],
         ["Dustin", "Henderson", "Science Enthusiast"]
     ]
-    week02 = Week02()
     print(f"ORIGINAL LIST = {_ST_CHARACTERS}")
 
     # testing adds:
-    # week02.add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    # week02.add_unique_first_name("Mike", "Wheeler", "annoying kid", _ST_CHARACTERS)
-    # week02.add_unique_last_name("Holly", "Wheeler", "goat", _ST_CHARACTERS)
+    # add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    # add_unique_first_name("Mike", "Wheeler", "annoying kid", _ST_CHARACTERS)
+    # add_unique_last_name("Holly", "Wheeler", "goat", _ST_CHARACTERS)
 
     # testing removes:
-    # week02.remove_first_name("Jim", _ST_CHARACTERS)
+    # remove_first_name("Jim", _ST_CHARACTERS)
     
-    # week02.add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    # week02.add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
-    # print(week02.remove_all_first_name("Yuri", _ST_CHARACTERS))
+    # add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    # add("Yuri", "Ismaylov", "Smuggler", _ST_CHARACTERS)
+    # remove_all_first_name("Yuri", _ST_CHARACTERS)
 
     print(f"FINAL LIST = {_ST_CHARACTERS}")
 
+main()
